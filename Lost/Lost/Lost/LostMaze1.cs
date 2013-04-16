bdee88677b4019c535fb01f41b0bf427abc407f1
Jon Ostwald <ostwald74029@gotoltc.edu>
2013-04-16 14:45:02 -0500

    fhasjkfhh

---
+++ Lost/Lost/Lost/LostMaze1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lost
+{
+    class LostMaze1
+    {
+
+
+        block1 = new Rectangle (a [0,0], a [0,1];
+        block2 = new Rectangle (a [1,0], a [1,1];
+        block3 = new Rectangle (a [2,0], a [2,1];
+        block4 = new Rectangle (a [3,0], a [3,1];
+        block5 = new Rectangle (a [4,0], a [4,1];
+        block6 = new Rectangle (a [5,0], a [5,1];
+        block7 = new Rectangle (a [6,0], a [6,1];
+        block8 = new Rectangle (a [7,0], a [7,1];
+        block9 = new Rectangle (a [8,0], a [8,1];
+        block10 = new Rectangle (a [9,0], a [9,1];
+        block11 = new Rectangle (a [10,0], a [10,1];
+        block12 = new Rectangle (a [11,0], a [11,1];
+        
+       
+        block13 = new Rectangle (a [12,0], a [12,1];
+        block14 = new Rectangle (a [13,0], a [13,1];
+        block15 = new Rectangle (a [14,0], a [14,1];
+        block16 = new Rectangle (a [15,0], a [15,1];
+        block17 = new Rectangle (a [16,0], a [16,1];
+        block18 = new Rectangle (a [17,0], a [17,1];
+           block19 = new Rectangle (a [18,0], a [18,1];
+        block20 = new Rectangle (a [19,0], a [19,1];
+        block21 = new Rectangle (a [20,0], a [20,1];
+        block22 = new Rectangle (a [21,0], a [21,1];
+        block23 = new Rectangle (a [22,0], a [22,1];
+        block24 = new Rectangle (a [23,0], a [23,1];
+        block25 = new Rectangle (a [24,0], a [24,1];
+        block26 = new Rectangle (a [25,0], a [25,1];
+        block27 = new Rectangle (a [26,0], a [26,1];
+        block28 = new Rectangle (a [27,0], a [27,1];
+        block29 = new Rectangle (a [28,0], a [28,1];
+        block30 = new Rectangle (a [29,0], a [29,1];
+        
+       
+        block31 = new Rectangle (a [30,0], a [30,1];
+        block32 = new Rectangle (a [31,0], a [31,1];
+        block33 = new Rectangle (a [32,0], a [32,1];
+        block34 = new Rectangle (a [33,0], a [33,1];
+        block35 = new Rectangle (a [34,0], a [34,1];
+        block36 = new Rectangle (a [35,0], a [35,1];
+              block37 = new Rectangle (a [36,0], a [36,1];
+        block38 = new Rectangle (a [37,0], a [37,1];
+        block39 = new Rectangle (a [38,0], a [38,1];
+        block40 = new Rectangle (a [39,0], a [39,1];
+        block41 = new Rectangle (a [40,0], a [40,1];
+        block42 = new Rectangle (a [41,0], a [41,1];
+        block43 = new Rectangle (a [42,0], a [42,1];
+        block44 = new Rectangle (a [43,0], a [43,1];
+        block45 = new Rectangle (a [44,0], a [44,1];
+        block46 = new Rectangle (a [45,0], a [45,1];
+        block47 = new Rectangle (a [46,0], a [46,1];
+        block48 = new Rectangle (a [47,0], a [47,1];
+        
+       
+        block49 = new Rectangle (a [48,0], a [48,1];
+        block50 = new Rectangle (a [49,0], a [49,1];
+        block51 = new Rectangle (a [50,0], a [50,1];
+        block52 = new Rectangle (a [51,0], a [51,1];
+        block53 = new Rectangle (a [52,0], a [52,1];
+        block54 = new Rectangle (a [53,0], a [53,1];
+        block55 = new Rectangle (a [54,0], a [54,1];
+        block56 = new Rectangle (a [55,0], a [55,1];
+        block57 = new Rectangle (a [56,0], a [56,1];
+        block58 = new Rectangle (a [57,0], a [57,1];
+        block59 = new Rectangle (a [58,0], a [58,1];
+        block60 = new Rectangle (a [59,0], a [59,1];
+        block61 = new Rectangle (a [60,0], a [60,1];
+        block62 = new Rectangle (a [61,0], a [61,1];
+        block63 = new Rectangle (a [62,0], a [62,1];
+        block64 = new Rectangle (a [63,0], a [63,1];
+        block65 = new Rectangle (a [64,0], a [64,1];
+        block66 = new Rectangle (a [65,0], a [65,1];
+        
+       
+        block67 = new Rectangle (a [66,0], a [66,1];
+        block68 = new Rectangle (a [67,0], a [67,1];
+        block69 = new Rectangle (a [68,0], a [68,1];
+        block70 = new Rectangle (a [69,0], a [69,1];
+        block71 = new Rectangle (a [70,0], a [70,1];
+        block72 = new Rectangle (a [71,0], a [71,1];
+        block73 = new Rectangle (a [72,0], a [72,1];
+        block74 = new Rectangle (a [73,0], a [73,1];
+
+
+
+
+
+
+    }
+}
